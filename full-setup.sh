6d9d37b93744f67b66f165cabd593d6c0c182e1e
brajasekharreddy <raj479.mits@gmail.com>
2023-02-12 22:20:45 +0530

    Added AWS parameters for passwords and full roboshop setup using ansible

--- full-setup.sh
+++ full-setup.sh
@@ -1,5 +1,6 @@
 for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
 
-  ansible-playbook -i ${component}.rajdevops.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
+  //ansible-playbook -i ${component}.rajdevops.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
+    ansible-playbook -i ${component}-dev.rajdevops.online, roboshop.yml -e ansible_user=centos -e ansible_password=DevOps321 -e ROLE_NAME=${component} -e env=dev
 
 done
